875ad04903086ab4c06f2a69ffbf3bde7a433183
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2025-04-29 20:50:13 -0500

    chore: format

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -2,7 +2,11 @@ import pygame
 import esper
 import json
 
-from src.create.prefab_creator import create_input_player, create_logo, create_text_interface
+from src.create.prefab_creator import (
+    create_input_player,
+    create_logo,
+    create_text_interface,
+)
 from src.ecs.components.c_input_command import CInputCommand, CommandPhase
 from src.ecs.systems.s_input_player import system_input_player
 from src.ecs.systems.s_rendering import system_rendering
@@ -12,27 +16,32 @@ class GameEngine:
     def __init__(self) -> None:
         # Load configuration from JSON file
         self._load_json()
-        
+
         # Initialize Pygame
         pygame.init()
         pygame.display.set_caption(self.window_cfg["title"])
-        self.screen = pygame.display.set_mode((self.window_cfg["size"]["w"], self.window_cfg["size"]["h"]), 
-                                              pygame.SCALED)
-        self.bg_color = pygame.Color(self.window_cfg["bg_color"]["r"], 
-                                     self.window_cfg["bg_color"]["g"], 
-                                     self.window_cfg["bg_color"]["b"])
-        
+        self.screen = pygame.display.set_mode(
+            (self.window_cfg["size"]["w"], self.window_cfg["size"]["h"]), pygame.SCALED
+        )
+        self.bg_color = pygame.Color(
+            self.window_cfg["bg_color"]["r"],
+            self.window_cfg["bg_color"]["g"],
+            self.window_cfg["bg_color"]["b"],
+        )
+
         # Initialize the clock
         self.clock = pygame.time.Clock()
         self.framerate = self.window_cfg["framerate"]
         self.delta_time = 0
-        
+
         self.is_paused = False
         self.is_running = False
-        self.mode = 'screen_1'
-        
+        self.mode = "screen_1"
+
         # Initialize the world
         self.ecs_world = esper.World()
+
+        self.screen_entities = []
 
     def _load_json(self):
         with open("./assets/cfg/window.json", encoding="utf-8") as window_file:
@@ -50,30 +59,32 @@ class GameEngine:
             self._draw()
         self._clean()
 
-
-    def _create_multiple(self, interface_cfg:dict, type:str):
+    def _create_multiple(self, interface_cfg: dict, type: str):
         entity_list = []
         for name in interface_cfg[type]:
             if "text" in interface_cfg[type][name]:
-                entity_list.append(create_text_interface(self.ecs_world, self.interface_cfg[type][name]))
+                entity_list.append(
+                    create_text_interface(
+                        self.ecs_world, self.interface_cfg[type][name]
+                    )
+                )
             elif "image" in interface_cfg[type][name]:
-                entity_list.append(create_logo(self.ecs_world, self.interface_cfg[type][name]))
-        
+                entity_list.append(
+                    create_logo(self.ecs_world, self.interface_cfg[type][name])
+                )
+
         return entity_list
-    
+
     def _create(self):
-        
+
         create_input_player(self.ecs_world)
-        
+
         # Create constants
         self.constants_entities = self._create_multiple(self.interface_cfg, "constants")
-        
+
         # Create screen 1
         self.screen_entities = self._create_multiple(self.interface_cfg, "screen_1")
-    
-    
-    
-    
+
     def _calculate_time(self):
         self.clock.tick(self.framerate)
         self.delta_time = self.clock.get_time() / 1000.0
@@ -95,15 +106,14 @@ class GameEngine:
     def _clean(self):
         self.ecs_world.clear_database()
         pygame.quit()
-    
-    def _do_action(self, c_input:CInputCommand):
+
+    def _do_action(self, c_input: CInputCommand):
         if c_input.name == "NEXT_SCREEN":
             if c_input.phase == CommandPhase.START:
                 if self.mode == "screen_1":
                     self.mode = "screen_2"
                     for entity in self.screen_entities:
                         self.ecs_world.delete_entity(entity)
-                    self.screen_entities = self._create_multiple(self.interface_cfg, "screen_2")
-                
-                
-        
+                    self.screen_entities = self._create_multiple(
+                        self.interface_cfg, "screen_2"
+                    )
