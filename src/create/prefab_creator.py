a7642eafa54499bd1167fd2ad0c56927803fd8c9
Sofia Velasquez Marin <s.velasquezm2@uniandes.edu.co>
2025-04-29 20:41:14 -0500

    Refactor interface configuration and enhance game engine functionality with new logo and input handling

--- src/create/prefab_creator.py
+++ src/create/prefab_creator.py
@@ -1,8 +1,10 @@
 import pygame
 import esper
 
+from src.ecs.components.c_input_command import CInputCommand
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
+from src.ecs.components.c_velocity import CVelocity
 from src.engine.service_locator import ServiceLocator
 
 def create_text(world:esper.World, text:str, font:pygame.font.Font, color:pygame.Color, pos:pygame.Vector2, center: bool = False) -> int:
@@ -16,16 +18,45 @@ def create_text(world:esper.World, text:str, font:pygame.font.Font, color:pygame
     world.add_component(text_entity, CSurface.from_text(text, font, color))
     return text_entity
 
-def create_text_interface(world:esper.World, interface_info:dict, type:str):
+def create_sprite(ecs_world: esper.World, pos: pygame.Vector2, vel: pygame.Vector2, surface: pygame.Surface, center: bool = False) -> int:
+    if center:
+        sprite_rect = surface.get_rect()
+        pos = pygame.Vector2(pos.x - sprite_rect.width / 2, pos.y - sprite_rect.height / 2)
+        print(f"pos: {pos}")
+    sprite_entity = ecs_world.create_entity()
+    ecs_world.add_component(sprite_entity, 
+                            CTransform(pos))
+    ecs_world.add_component(sprite_entity, 
+                            CVelocity(vel))
+    ecs_world.add_component(sprite_entity, 
+                            CSurface.from_surface(surface))
+    return sprite_entity
+
+def create_text_interface(world:esper.World, interface_info:dict):
     font = ServiceLocator.texts_service.get(interface_info["font"], 
-                                            interface_info[type]["size"])
+                                            interface_info["size"])
     
-    color = pygame.Color(interface_info[type]["color"]["r"],
-                         interface_info[type]["color"]["g"],
-                         interface_info[type]["color"]["b"])
+    color = pygame.Color(interface_info["color"]["r"],
+                         interface_info["color"]["g"],
+                         interface_info["color"]["b"])
     
-    pos = pygame.Vector2(interface_info[type]["pos"]["x"], interface_info[type]["pos"]["y"])
+    pos = pygame.Vector2(interface_info["pos"]["x"], interface_info["pos"]["y"])
     
-    center = interface_info[type].get("center", False)
+    center = interface_info.get("center", False)
     
-    return create_text(world, interface_info[type]["text"], font, color, pos, center)
+    return create_text(world, interface_info["text"], font, color, pos, center)
+
+def create_logo(world:esper.World, logo_info:dict) -> int:
+    surface = ServiceLocator.images_service.get(logo_info["image"])
+    pos = pygame.Vector2(logo_info["pos"]["x"], logo_info["pos"]["y"])
+    vel = pygame.Vector2(0,0)
+    center = logo_info.get("center", False)
+    return create_sprite(world, pos, vel, surface, center)
+
+
+def create_input_player(ecs_world:esper.World) :
+    input_next = ecs_world.create_entity()
+    
+    
+    ecs_world.add_component(input_next, CInputCommand("NEXT_SCREEN", pygame.K_RETURN))
+    
--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -2,8 +2,8 @@ import pygame
 import esper
 import json
 
-from src.create.prefab_creator import create_text_interface
-from src.ecs.components.c_input_command import CInputCommand
+from src.create.prefab_creator import create_input_player, create_logo, create_text_interface
+from src.ecs.components.c_input_command import CInputCommand, CommandPhase
 from src.ecs.systems.s_input_player import system_input_player
 from src.ecs.systems.s_rendering import system_rendering
 
@@ -29,6 +29,7 @@ class GameEngine:
         
         self.is_paused = False
         self.is_running = False
+        self.mode = 'screen_1'
         
         # Initialize the world
         self.ecs_world = esper.World()
@@ -49,19 +50,32 @@ class GameEngine:
             self._draw()
         self._clean()
 
-    def _create(self):
-        create_text_interface(self.ecs_world, self.interface_cfg, "play_prompt")
-        create_text_interface(self.ecs_world, self.interface_cfg, "title")
-        create_text_interface(self.ecs_world, self.interface_cfg, "insert_coin")
-        create_text_interface(self.ecs_world, self.interface_cfg, "try_game")
-        create_text_interface(self.ecs_world, self.interface_cfg, "highscore")
-        create_text_interface(self.ecs_world, self.interface_cfg, "copyright")
-        create_text_interface(self.ecs_world, self.interface_cfg, "1-UP")
-        create_text_interface(self.ecs_world, self.interface_cfg, "2-UP")
-        create_text_interface(self.ecs_world, self.interface_cfg, "credit")
-        create_text_interface(self.ecs_world, self.interface_cfg, "00")
-
+    
         
+
+    
+    def _create_multiple(self, interface_cfg:dict, type:str):
+        entity_list = []
+        for name in interface_cfg[type]:
+            if "text" in interface_cfg[type][name]:
+                entity_list.append(create_text_interface(self.ecs_world, self.interface_cfg[type][name]))
+            elif "image" in interface_cfg[type][name]:
+                entity_list.append(create_logo(self.ecs_world, self.interface_cfg[type][name]))
+        
+        return entity_list
+    
+    def _create(self):
+        
+        create_input_player(self.ecs_world)
+        
+        # Create constants
+        self.constants_entities = self._create_multiple(self.interface_cfg, "constants")
+        
+        # Create screen 1
+        self.screen_entities = self._create_multiple(self.interface_cfg, "screen_1")
+        # Create screen 2
+        # self.screen2_entities = self._create_multiple(self.interface_cfg, "screen_2")
+    
     def _calculate_time(self):
         self.clock.tick(self.framerate)
         self.delta_time = self.clock.get_time() / 1000.0
@@ -73,7 +87,7 @@ class GameEngine:
                 self.is_running = False
 
     def _update(self):
-        pass
+        self.ecs_world._clear_dead_entities()
 
     def _draw(self):
         self.screen.fill(self.bg_color)
@@ -85,5 +99,15 @@ class GameEngine:
         pygame.quit()
     
     def _do_action(self, c_input:CInputCommand):
-        pass
+        if c_input.name == "NEXT_SCREEN":
+            if c_input.phase == CommandPhase.START:
+                print("NEXT_SCREEN")
+                
+                if self.mode == "screen_1":
+                    self.mode = "screen_2"
+                    for entity in self.screen_entities:
+                        self.ecs_world.delete_entity(entity)
+                    self.screen_entities = self._create_multiple(self.interface_cfg, "screen_2")
+                
+                
         
